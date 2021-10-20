6f486a2542997d231f621222e799c7f86b0958ff
ArdaAlp <82732641+ArdaAlp@users.noreply.github.com>
2021-10-20 23:00:33 +0300

    Add files via upload

---
+++ api/error.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+
+<!-- META TAGS -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta charset="UTF-8">
+<meta name="author" content="Arda Alp ">
+
+<!-- STYLE SOURCE -->
+<link rel="stylesheet" href="style.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+<!-- FONT AWESOME -->
+<script src="https://kit.fontawesome.com/b1d04c92bf.js" crossorigin="anonymous"></script>
+
+<!-- SCRIPT SOURCE (JS + JQUERY) -->
+<script src=""></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<title>Get My Script</title>
+</head>
+
+<body>
+
+<header>
+<h1>Script Not Found</h1>
+</header>
+
+<a class href="index.html">Search new script...</a>
+
+<footer>
+<p>Created By Arda Alp Copyright© Since 2021-2022</p>
+</footer>
+
+</body>
+</html>
